6e80ecadf79ede337382ceb22eec70c063970639
Mark Ni <117698602+hahawang1986@users.noreply.github.com>
2023-02-10 13:27:08 +1030

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -4,4 +4,5 @@ git checkout -b branch2 origin/branch2
 git merge branch3
 git add .
 git commit -m "merge branch3 with brance2"
-git branch -d brance3
+git branch -d branch3
+git commit -m "delete b3" 
