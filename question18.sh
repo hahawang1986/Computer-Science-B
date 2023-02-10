4f88b2891ad0b2ba96666a27014a0121ec994406
Mark Ni <117698602+hahawang1986@users.noreply.github.com>
2023-02-10 13:48:02 +1030

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -1,4 +1,4 @@
-cd git-practice-04
+cd ~/git-practice-04
 #ranches=$(git branch -r | grep -E */ready*)
 #or branch in $branches; do
 #git checkout $branch
@@ -11,11 +11,14 @@ git checkout main
 git merge ready1
 git merge ready2
 git merge ready3
-git branch -d $(git branch -v | grep -E ready*)
+#git branch -d $(git branch -v | grep -E ready*)
+git branch -d ready1
+git branch -d ready2
+git branch -d ready3
 git commit -am "delete all branches with ready"
 git branch -r | grep -E */update*
 git checkout update1
+git merge main
 git checkout update2
 git merge main
-git checkout update1
-git merge main
+
