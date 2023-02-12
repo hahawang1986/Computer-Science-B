dfd22f264c6cead59bb90a7114b5cfa323bd8c22
Mark Ni <117698602+hahawang1986@users.noreply.github.com>
2023-02-12 17:46:32 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -2,7 +2,7 @@
 git branch branch3
 git checkout branch3
 git rm *.sh
-git commit -am "rm all .sh files"
+git commit -am "rm all files"
 touch file13.txt
 git add file13.txt
 git commit -m "create file13.txt" file13.txt
